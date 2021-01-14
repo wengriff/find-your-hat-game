b4c4fd4f6bcf591aad3c32b4340c708faa2146e6
wengriff <adrian.somor2@gmail.com>
2021-01-14 12:27:43 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,52 @@
+# Find Your Hat Game
+> A simple terminal game
+
+## Table of contents
+* [General info](#general-info)
+* [Preview](#preview)
+* [How to play](#how-to-play)
+* [Technologies](#technologies)
+* [TODO](#todo)
+* [Status](#status)
+* [Contact](#cotact)
+
+## General info
+
+A terminal game in which you win if you find your hat
+
+## Preview
+
+![Preview of the Game](./find-your-hat-demo.gif)
+
+## How to play
+
+To play you will need to download the following:
+* [Setting Up Node Locally](https://www.codecademy.com/articles/setting-up-node-locally)
+* [Command Line Interface Setup](https://www.codecademy.com/articles/command-line-setup)
+
+After that, just navigate to the folder where you downloaded the game and type: node main.js to start playing the game!
+
+The game will ask you which way you want to move
+* Press 'W' and hit 'Enter' to go up
+* Press 'S' and hit 'Enter' to go down
+* Press 'D' and hit 'Enter' to go right
+* Press 'A' and hit 'Enter' to go left
+
+Avoid falling into the hole or going outside of the playing field.
+
+## Technologies
+Project is created with:
+* JavaScript
+
+## TODO
+* Have a character start on a random location.
+* Create a "hard mode" (holes are added after certain turns)
+* Improve graphics using [terminal-kit](https://github.com/cronvel/terminal-kit)
+* Create a field validator to ensure the field generated can actually be solved using [Maze Solving Algorithm](https://en.wikipedia.org/wiki/Maze_solving_algorithm)
+* Add a how-to-play introduction inside the game for a new player
+
+## Status
+Project is not finished
+
+## Contact
+Created by @wengriff
---
+++ main.js
@@ -0,0 +1,145 @@
+const prompt = require('prompt-sync')({sigint: true});
+
+class Field {
+  constructor(arr) {
+    this._arr = arr;
+    this._playerX = 0;
+    this._playerY = 0;
+  }
+
+  print() {
+    for(let x = 0; x < 15; x++){
+        console.log('\n');
+    }
+    this._arr[this._playerX][this._playerY] = '*';
+    for(let i = 0; i < this._arr.length; i++) {
+      console.log(this._arr[i].join(''));
+    }
+    console.log('\n');
+  }
+
+  findHat() {
+    let hatIndex = [];
+    for(let i = 0; i < this._arr.length; i++){
+      for(let j = 0; j < this._arr[i].length; j++) {
+        if(this._arr[i][j] === '^')
+          hatIndex.push(i, j);
+      }
+    }
+    return hatIndex;
+  }
+
+  result() {
+    let hatIndex = this.findHat();
+    let starIndex = [this._playerX, this._playerY];
+
+    if(hatIndex[0] === starIndex[0] && hatIndex[1] === starIndex[1]) {
+      return 'Congrats! You found your hat.';
+    }
+
+    if((starIndex[0] > this._arr.length - 1) ||
+      (starIndex[0] < 0) ||
+      (starIndex[1] > this._arr[0].length - 1) ||
+      (starIndex[1] < 0)) {
+      return 'You are out of bound';
+    }
+
+    if(this._arr[starIndex[0]][starIndex[1]] === 'O') {
+      return 'Sorry, you fell down a hole.';
+    }
+    return false;
+  }
+
+  move() {
+    let move = prompt('Which way? ');
+
+    if(move === 'w') {
+      this._playerX -= 1;
+    }
+
+    if(move === 's') {
+      this._playerX += 1;
+    }
+
+    if(move === 'd') {
+      this._playerY += 1;
+    }
+
+    if(move === 'a') {
+      this._playerY -= 1;
+    }
+  }
+
+  static generateField(height, width, percentage) {
+
+    let arr = [];
+    const hat = '^';
+    let hatPosition = [];
+    const hole = 'O';
+    let holesIndex = [];
+    const fieldCharacter = '░';
+    const pathCharacter = '*';
+    let starPosition = [0, 0]
+
+    let randomHatPositionX = Math.floor(Math.random() * height);
+    let randomHatPositionY = Math.floor(Math.random() * width);
+
+    let numberOfHoles = Math.floor((percentage / 100) * (height * width));
+
+    while((randomHatPositionX === starPosition[0]) && (randomHatPositionY === starPosition[1])) {
+      randomHatPositionX = Math.floor(Math.random() * height);
+      randomHatPositionY = Math.floor(Math.random() * width);
+    }
+
+    hatPosition = [randomHatPositionX, randomHatPositionY];
+
+    for(let i = 0; i < height; i++) {
+      arr[i] = [];
+      for(let j = 0; j < width; j++) {
+        arr[i][j] = fieldCharacter;
+      }
+    }
+
+    for(let i = 0; i <= numberOfHoles; i++) {
+      holesIndex[i] = [];
+      for(let j = 0; j <= 1; j++) {
+        if(j === 0){
+          holesIndex[i][j] = Math.floor(Math.random() * height);
+          while(holesIndex[i][j] === starPosition[0] && holesIndex[i][j] === hatPosition[0]){
+            holesIndex[i][j] = Math.floor(Math.random() * height);
+          }
+        }
+        if(j === 1){
+          holesIndex[i][j] = Math.floor(Math.random() * width);
+          while(holesIndex[i][j] === starPosition[1] && holesIndex[i][j] === hatPosition[1]){
+            holesIndex[i][j] = Math.floor(Math.random() * width);
+          }
+        }
+      }
+    }
+
+    for(let i = 0; i < numberOfHoles; i++) {
+      for(let j = 0; j < 1; j++) {
+        arr[holesIndex[i][j]][holesIndex[i][j+1]] = hole;
+      }
+    }
+
+    arr[hatPosition[0]][hatPosition[1]] = hat;
+
+    return arr;
+  }
+
+  playGame() {
+    let result = false;
+    while(result === false) {
+      this.print();
+      this.move();
+      result = this.result();
+    }
+    console.log(result);
+  }
+}
+
+const myField = new Field(Field.generateField(20, 20, 20));
+
+myField.playGame();
